1d0934c15b94131542b407b851b4e42206ff573f
Cesar <cesar6134@gmail.com>
2014-07-21 23:18:39 +0800

    Modify the Meteor

--- meteor/client/client.js
+++ meteor/client/client.js
@@ -1,3 +1,11 @@
+/**
+* Created with URLcomment.
+* User: cesar2535
+* Date: 2014-07-19
+* Time: 04:43 AM
+* To change this template use Tools | Templates.
+*/
+
 Template.hello.greeting = function () {
     return "Welcome to meteor.";
 };
@@ -5,8 +13,7 @@ Template.hello.greeting = function () {
 Template.hello.events({
     'click input': function () {
         // template data, if any, is available in 'this'
-        if (typeof console !== 'undefined')
-            console.log("You pressed the button");
+        // if (typeof console !== 'undefined')
+        console.log("You pressed the button");
     }
 });
-
--- meteor/server/server.js
+++ meteor/server/server.js
@@ -1,3 +1,11 @@
+/**
+* Created with URLcomment.
+* User: cesar2535
+* Date: 2014-07-19
+* Time: 04:44 AM
+* To change this template use Tools | Templates.
+*/
+
 Meteor.startup(function () {
     // code to run on server at startup
 });
