cc6250576cc503f97db1f2da9551e6f123601131
cesar2535 <cesar6134@gmail.com>
2014-07-24 22:18:15 +0800

    Create the Collection, and remove the autopublish package

--- meteor/server/fixture.js
+++ meteor/server/fixture.js
@@ -1,23 +1,19 @@
-var postData = [
-    {
-        title: 'Interoducing Telescope',
+if (Posts.find().count() === 0) {
+    Posts.insert({
+        title: 'Introducing Telescope',
         author: 'Sacha Greif',
         url: 'http://sachagreif.com/introducing-telescope/'
-    },
-    {
+    });
+    
+    Posts.insert({
         title: 'Meteor',
         author: 'Tom Coleman',
         url: 'http://meteor.com'
-    },
-    {
+    });
+    
+    Posts.insert({
         title: 'The Meteor Book',
         author: 'Tom Coleman',
         url: 'http://themeteorbook.com'
-    }
-];
-
-Template.postsList.helpers({
-    posts: function() {
-        return Posts.find();
-    }
-});
+    });
+}
