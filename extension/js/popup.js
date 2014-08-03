3750b63aa638b1f2a1fc1fe8f8058a95e08308a9
Cesar <cesar6134@gmail.com>
2014-08-03 17:42:44 +0800

    Changed the position of functions

--- extension/js/popup.js
+++ extension/js/popup.js
@@ -1,14 +1,14 @@
 (function() {
-//    var ddp = new MeteorDdp("ws://localhost:3000/websocket");
-//    
-//    ddp.connect().then(function() {
-//        ddp.subscribe('myBookPosts');
-//    });
-    
     chrome.tabs.query({ 
         active: true,
         lastFocusedWindow: true
     }, function(tabArray) {
         document.getElementById('currentLink').innerHTML = tabArray[0].url;
     });
+    
+//    var ddp = new MeteorDdp("ws://localhost:3000/websocket");
+//    
+//    ddp.connect().then(function() {
+//        ddp.subscribe('myBookPosts');
+//    });
 })();
