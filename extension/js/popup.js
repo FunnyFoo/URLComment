618e3db46a64eb9ba7cdde4a41b60b707a7a52a3
Cesar <cesar6134@gmail.com>
2014-08-10 23:12:24 +0800

    Beautified codes and add bootstrap-3 into chrome extension

--- extension/js/popup.js
+++ extension/js/popup.js
@@ -1,21 +1,21 @@
-(function() {
-  var tabUrl = null;
+(function () {
+	var tabUrl = null;
 
-  chrome.tabs.query({ 
-    active: true,
-    lastFocusedWindow: true
-  }, function(tabArray) {
-    tabUrl = tabArray[0].url;
-    document.getElementById('currentLink').innerHTML = tabUrl;
-  });
-    
-  // var ddp = new MeteorDdp("ws://localhost:3000/websocket");
-   
-  // ddp.connect().then(function() {
-  //   ddp.subscribe('myBookPosts');
+	chrome.tabs.query({
+		active: true,
+		lastFocusedWindow: true
+	}, function (tabArray) {
+		tabUrl = tabArray[0].url;
+		document.getElementById('currentLink').innerHTML = tabUrl;
+	});
 
-  //   ddp.watch('myBookPosts', function(changeDoc, message) {
-      
-  //   });
-  // });
+	// var ddp = new MeteorDdp("ws://localhost:3000/websocket");
+
+	// ddp.connect().then(function() {
+	//   ddp.subscribe('myBookPosts');
+
+	//   ddp.watch('myBookPosts', function(changeDoc, message) {
+
+	//   });
+	// });
 })();
--- meteor/server/server.js
+++ meteor/server/server.js
@@ -1,44 +1,44 @@
 var samplePostsData = [{
-    "user_id": 		"",		// get ID from twitter, facebook, or anonymous
-    "name": 	"Anonymous",		// 
-    "rate": {
-        "0": "", 			// neutral comment
-        "1": "", 			// positive comment
-        "-1": ""			// negative comment
-    },
-    "comment": 	"",
-    "date": 	"Sat Jul 19 2014",
-    "url": 		"",
-    "ip": 		"",
-    "device": 	"",
-    "public": 	true,	// public or not by boolean
-    "counter": 	""		// times of view for the comment
+	"user_id": "", // get ID from twitter, facebook, or anonymous
+	"name": "Anonymous", // 
+	"rate": {
+		"0": "", // neutral comment
+		"1": "", // positive comment
+		"-1": "" // negative comment
+	},
+	"comment": "",
+	"date": "Sat Jul 19 2014",
+	"url": "",
+	"ip": "",
+	"device": "",
+	"public": true, // public or not by boolean
+	"counter": "" // times of view for the comment
 }, {
-    "date": "Sat Jul 19 2014",
-    "name": "You",
-    "comment": "wtf"
+	"date": "Sat Jul 19 2014",
+	"name": "You",
+	"comment": "wtf"
 }];
 
-Meteor.startup(function() {
-  // code to run on server at startup
-  Meteor.publish('myBookPosts', function() {
-   	return Posts.find();
+Meteor.startup(function () {
+	// code to run on server at startup
+	Meteor.publish('myBookPosts', function () {
+		return Posts.find();
 	});
 });
 
 Meteor.methods({
-  postComment: function(name, rate, comment, url, pub) {
-    var post = {
-      'name': name,
-      'rate': rate,
-      'comment': comment,
-      'date': new Date().toDateString(),
-      'url': url,
-      'ip': '',
-      'device': '',
-      'public': pub
-    }
+	postComment: function (name, rate, comment, url, pub) {
+		var post = {
+			'name': name,
+			'rate': rate,
+			'comment': comment,
+			'date': new Date().toDateString(),
+			'url': url,
+			'ip': '',
+			'device': '',
+			'public': pub
+		}
 
-    post._id = Posts.insert(post);
-  }
+		post._id = Posts.insert(post);
+	}
 });
