f49f7c4e33d345ad88391ec6201e7f3c7fc3e010
cesar2535 <cesar6134@gmail.com>
2014-08-10 21:37:41 +0800

    Test extension

--- extension/js/popup.js
+++ extension/js/popup.js
@@ -1,14 +1,21 @@
 (function() {
-    chrome.tabs.query({ 
-        active: true,
-        lastFocusedWindow: true
-    }, function(tabArray) {
-        document.getElementById('currentLink').innerHTML = tabArray[0].url;
-    });
+  var tabUrl = null;
+
+  chrome.tabs.query({ 
+    active: true,
+    lastFocusedWindow: true
+  }, function(tabArray) {
+    tabUrl = tabArray[0].url;
+    document.getElementById('currentLink').innerHTML = tabUrl;
+  });
     
-//    var ddp = new MeteorDdp("ws://localhost:3000/websocket");
-//    
-//    ddp.connect().then(function() {
-//        ddp.subscribe('myBookPosts');
-//    });
+  // var ddp = new MeteorDdp("ws://localhost:3000/websocket");
+   
+  // ddp.connect().then(function() {
+  //   ddp.subscribe('myBookPosts');
+
+  //   ddp.watch('myBookPosts', function(changeDoc, message) {
+      
+  //   });
+  // });
 })();
