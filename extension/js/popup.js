3093696f5f1c788ff38b3799fd91c0a1386b0967
Jiminy Cricket <jiminy.upon.star@gmail.com>
2014-08-13 00:28:48 +0800

    layout and rextarea auto resize

--- extension/js/popup.js
+++ extension/js/popup.js
@@ -1,21 +1,30 @@
-(function () {
-	var tabUrl = null;
+// (function () {
+// 	var tabUrl = null;
 
-	chrome.tabs.query({
-		active: true,
-		lastFocusedWindow: true
-	}, function (tabArray) {
-		tabUrl = tabArray[0].url;
-		document.getElementById('currentLink').innerHTML = tabUrl;
-	});
+// 	chrome.tabs.query({
+// 		active: true,
+// 		lastFocusedWindow: true
+// 	}, function (tabArray) {
+// 		tabUrl = tabArray[0].url;
+// 		document.getElementById('currentLink').innerHTML = tabUrl;
+// 	});
 
-	// var ddp = new MeteorDdp("ws://localhost:3000/websocket");
+// 	// var ddp = new MeteorDdp("ws://localhost:3000/websocket");
 
-	// ddp.connect().then(function() {
-	//   ddp.subscribe('myBookPosts');
+// 	// ddp.connect().then(function() {
+// 	//   ddp.subscribe('myBookPosts');
 
-	//   ddp.watch('myBookPosts', function(changeDoc, message) {
+// 	//   ddp.watch('myBookPosts', function(changeDoc, message) {
 
-	//   });
-	// });
-})();
+// 	//   });
+// 	// });
+// })();
+
+$(document).ready(function(){
+	var opts = {
+  	context: $('.write-command')
+  , animate: true
+  , cloneClass: 'faketextarea'
+	};
+	$('.write').autogrow(opts);
+});
