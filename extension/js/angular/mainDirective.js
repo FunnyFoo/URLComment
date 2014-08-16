fceec02ae3fcd868068b8d04137b07907ffcbd86
purepennons <y80811@gmail.com>
2014-08-16 23:20:39 +0800

    just add a params for ngEnter.(no effect)

--- extension/js/angular/mainDirective.js
+++ extension/js/angular/mainDirective.js
@@ -3,7 +3,7 @@
 myapp.directive('ngEnter', function () {
   return {
     restrict: 'A',
-    link: function (scope, elem, attrs) {
+    link: function (scope, elem, attrs, ctrl) {
       elem.bind('keydown', function(event) {
         var code = event.keyCode || event.which;
         if (code === 13) {
