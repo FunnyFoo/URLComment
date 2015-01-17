3a8b91e5b5825abbb34126da9b3ca70ffcee772f
Cesar Chen <cesar6134@gmail.com>
2015-01-17 18:09:05 +0800

    Add Sass files

---
+++ config.rb
@@ -0,0 +1,16 @@
+
+require 'breakpoint'
+css_dir = "meteor/client/stylesheets" # by Fire.app 
+sass_dir = "sass" # by Fire.app 
+images_dir = "meteor/public/img" # by Fire.app 
+javascripts_dir = "javascripts" # by Fire.app 
+fireapp_coffeescripts_dir = "coffeescripts" # by Fire.app 
+fireapp_livescripts_dir = "livescripts" # by Fire.app 
+fireapp_minifyjs_on_build = false # by Fire.app 
+fireapp_always_report_on_build = true # by Fire.app 
+output_style = :expanded # by Fire.app 
+relative_assets = false # by Fire.app 
+line_comments = true # by Fire.app 
+sass_options = {:debug_info=>false} # by Fire.app 
+fireapp_coffeescript_options = {:bare=>false} # by Fire.app 
+fireapp_livescript_options = {:bare=>false} # by Fire.app 
