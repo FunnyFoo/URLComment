81e5d1804bdefd99ca22fd54744ec0585fe51b14
Cesar Chen <cesar6134@gmail.com>
2015-02-02 16:57:36 +0800

    Format time

--- meteor/client/templates/comments/comment_item.js
+++ meteor/client/templates/comments/comment_item.js
@@ -1,10 +1,17 @@
 Template.commentItem.helpers({
   submittedText: function () {
-    var time = new Date(this.submitted);
+    var time = new Date(parseInt(this.submitted));
     var year = time.getFullYear();
     var month = time.getUTCMonth() + 1;
     var date = time.getDate();
-    month = (month < 10)? '0' + month : month;
-    return month + '-' + date + '-' + year;
+    var hours = time.getHours();
+    var minutes = time.getMinutes();
+    var seconds = time.getSeconds();
+    month = (month < 10) ? '0' + month : month;
+    date = (date < 10) ? '0' + date : date;
+    hours = (hours < 10) ? '0' + hours : hours;
+    minutes = (minutes < 10) ? '0' + minutes : minutes;
+    seconds = (seconds < 10) ? '0' + seconds : seconds;
+    return year + '-' + month + '-' + date + ' ' + hours + ':' + minutes + ':' + seconds;
   }
 });
